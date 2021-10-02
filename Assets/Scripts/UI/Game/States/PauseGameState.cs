11e64e58d090e63d5f4162b757d39115da842c96
Coen Steuns <c.steuns@gmail.com>
2021-10-02 20:32:10 +0200

    pause menu

---
+++ Assets/Scripts/UI/Game/States/PauseGameState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGameState : PdaState
+{
+
+    [SerializeField]
+    private GameObject pauseGameMenu;
+
+    private float timeScaleAtEnter = 0;
+
+    private MenuOpenCheck menuInput = new MenuOpenCheck();
+
+    public override void Enter()
+    {
+        menuInput.Update();
+        timeScaleAtEnter = Time.timeScale;
+        Time.timeScale = 0;
+        pauseGameMenu.SetActive(true);
+    }
+
+    public override void Resume()
+    {
+        menuInput.Update();
+    }
+
+    public override void Leave()
+    {
+        Time.timeScale = timeScaleAtEnter;
+        pauseGameMenu.SetActive(false);
+    }
+
+    public override void Reason()
+    {
+        if(menuInput.Check()){
+            Machine.PopState();
+        } 
+    }
+
+
+}
