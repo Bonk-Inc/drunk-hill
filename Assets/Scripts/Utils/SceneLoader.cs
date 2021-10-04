0830545c3a8bd1a38ac94842163f1e7012359bb9
Coen Steuns <c.steuns@gmail.com>
2021-10-04 16:07:10 +0200

    generate scene loader

--- Assets/Scripts/Utils/SceneLoader.cs
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -6,22 +6,38 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
 
-    public static SceneLoader Instance {get; private set;}
+    private static SceneLoader instance;
+    public static SceneLoader Instance => GetInstance();
 
     [SerializeField]
     private LoadingScreen loadingScreen;
 
+    private static SceneLoader GetInstance(){
+        if(instance == null){
+            var gobj = new GameObject("Generated Scene Loader");
+            instance = gobj.AddComponent<SceneLoader>();
+        }
+        return instance;
+    }
+
     private void Awake() {
-        if(Instance != null){
+        if(instance != null){
             Destroy(gameObject);
             return;
         }
 
-        Instance = this;
+        instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
     public void LoadScene(string sceneName){
+
+        if (loadingScreen == null)
+        {
+            SceneManager.LoadScene(sceneName);
+            return;
+        }
+
         loadingScreen.OpenLoadingScreen(() => {
             var operation = SceneManager.LoadSceneAsync(sceneName);
             operation.completed += (op) => {
