d73ca45018b94512ee91f2d38873c02893381a53
Coen Steuns <c.steuns@gmail.com>
2021-10-04 19:26:53 +0200

    the pijn appel

---
+++ Assets/Scripts/Obstacles/Lorry/TargetMover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMover : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float driveTime = 0.5f;
+    [SerializeField]
+    private float startDelay = 5f;
+    
+
+    private void Start() {
+        StartCoroutine(MoveToTarget());
+    }
+
+    private IEnumerator MoveToTarget(){
+        var startPos = transform.position;
+        var targetPos = target.position;
+        float way = 0;
+        Debug.Log(way);
+        yield return new WaitForSecondsRealtime(startDelay - driveTime);
+        Debug.Log(way);
+        while ( way < 1)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, way);
+            way += Time.unscaledDeltaTime / driveTime;
+            yield return null;
+        }
+    }
+}
